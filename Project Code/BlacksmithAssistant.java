9ea00968dee0861bdbb809f54c7ad32306bf9516
fozobz <98126543+fozobz@users.noreply.github.com>
2024-02-16 15:21:07 -0800

    Building classes

---
+++ Project Code/BlacksmithAssistant.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+
+public class BlacksmithAssistant {
+
+    public static void main(String[] args) {
+        int w = 500;
+        int h = 500;
+        JFrame window = new JFrame();
+        window.setSize(w, h);
+        window.setTitle("Blacksmithing Curve Assistant");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+
+    }
+    
+}
---
+++ Project Code/GraphicalPreview.java
@@ -0,0 +1,5 @@
+import java.awt.*;
+
+public class GraphicalPreview extends Graphics2d{
+    
+}
